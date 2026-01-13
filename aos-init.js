ecf53f88e688f83d2fe41ff20748b4000c06c786
Ahsan <ahsanfiazpasha@gmail.com>
2026-01-13 18:41:54 +0500

    fds

--- aos-init.js
+++ aos-init.js
@@ -3,5 +3,8 @@ AOS.init({
 	duration: 900, // 0.9 seconds for slightly slow animation
 	easing: 'ease-in-out',
 	once: true,
-	mirror: true,
+	mirror: false, // Disable mirror to prevent scroll issues
+	offset: 50, // Start animation 50px before element is in view
+	anchorPlacement: 'top-bottom', // Trigger when top of element hits bottom of viewport
+	disable: false,
 });
